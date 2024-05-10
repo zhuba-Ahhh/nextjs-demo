f51d8a9b2b19cbc079270fc0c38c9534c5c8d8c5
zhuba-Ahhh <3477826311@qq.com>
2024-05-10 14:45:25 +0800

    build: :package: cancel Deploy

---
+++ app/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AnimatePresence } from "framer-motion";
+import type { AppProps } from "next/app";
+import "./globals.css";
+import { NextPageWithLayout } from "./page";
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(
+    <AnimatePresence initial={false} mode="popLayout">
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
+}
+
+export default MyApp;
---
+++ app/pages/index.tsx
@@ -0,0 +1,27 @@
+import CatCard from "../components/cards/cat/CatCard";
+import { mockCatCardProps } from "../components/cards/cat/CatCard.mocks";
+import PageTransition from "../components/layouts/pageTransition/PageTransition";
+import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
+import SidebarLayout from "../components/layouts/sidebar/SidebarLayout";
+import { NextPageWithLayout } from "./page";
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <section>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <CatCard {...mockCatCardProps.base} />
+    </section>
+  );
+};
+export default Home;
+
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout />
+      <PageTransition>{page}</PageTransition>
+    </PrimaryLayout>
+  );
+};
